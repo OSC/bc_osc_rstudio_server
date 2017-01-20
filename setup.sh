1e95406fa615ee749871dbea86bff17cad8c652e
Brian McMichael <bmcmichael@osc.edu>
2017-01-20 11:34:45 -0500

    add a setup script to download and chmod the proot binaries

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://static.proot.me/proot-x86_64 -P template/bin
+chmod +x template/bin/proot-x86_64
+wget http://static.proot.me/proot-x86 -P template/bin
+chmod +x template/bin/proot-x86
