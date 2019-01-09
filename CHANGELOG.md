105f1c9fddce278aa21d93584dca4d9d14723dd7
Morgan Rodgers <mrodgers@osc.edu>
2019-01-09 12:58:22 -0500

    Added JS to enforce the PPN limits per node type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+## [0.6.1] - 2018-01-09
+### Added
+- JS to enforce the PPN limits per node type
+
+## [0.6.0] - 2018-08-02
+### Added
+
+- A method to optionally set non-standard versions of R to be used with RStudio Server.
+- Added R 3.5.0 as an option
 
 ## [0.5.0] - 2018-04-18
 ### Added
@@ -70,7 +79,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Added
 - Initial release!
 
-[Unreleased]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.5.0...HEAD
+[Unreleased]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.6.1...HEAD
+[0.6.0]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.6.0...v0.6.1
+[0.6.0]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.5.0...v0.6.0
 [0.5.0]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.4.0...v0.5.0
 [0.4.0]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.3.0...v0.4.0
 [0.3.0]: https://github.com/OSC/bc_osc_rstudio_server/compare/v0.2.1...v0.3.0
---
+++ form.js
@@ -0,0 +1,57 @@
+'use strict'
+
+/**
+ * Fix num cores
+ */
+function fix_num_cores() {
+  let node_type_input = $('#batch_connect_session_context_node_type');
+  let node_type = node_type_input.val();
+  let num_cores_input = $('#num_cores');
+  
+  if(node_type === 'hugemem') {
+    set_ppn_owens_hugemem(num_cores_input);
+  } else {
+    set_ppn_owens_regular(num_cores_input);
+  }
+}
+
+/**
+ * Sets the PPN limits available for Owens hugemem nodes.
+ * 
+ * hugemem reservations are always assigned the full node
+ *
+ * @param      {element}  num_cores_input  The input for num_cores
+ */
+function set_ppn_owens_hugemem(num_cores_input) {
+  const NUM_CORES = 48;
+  num_cores_input.attr('max', NUM_CORES);
+  num_cores_input.attr('min', NUM_CORES);
+  num_cores_input.val(NUM_CORES);
+}
+
+/**
+ * Sets the PPN limits available for non hugemem Owens nodes.
+ *
+ * @param      {element}  num_cores_input  The input for num_cores
+ */
+function set_ppn_owens_regular(num_cores_input) {
+  const NUM_CORES = 28;
+  num_cores_input.attr('max', NUM_CORES);
+  num_cores_input.attr('min', 1);
+  num_cores_input.val(Math.min(NUM_CORES, num_cores_input.val()));
+}
+
+
+/**
+ * Change the maximum number of cores selected.
+ */
+function set_node_type_change_handler() {
+  let node_type_input = $('#batch_connect_session_context_node_type');
+  node_type_input.change(node_type_input, fix_num_cores);
+}
+
+$(document).ready(function() {
+  // Set the max value to be what was set in the last session
+  fix_num_cores();
+  set_node_type_change_handler();
+});
