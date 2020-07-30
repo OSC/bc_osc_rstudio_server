7da84b6bd527953a2d5652164b4d0c371705a7c7
Matthew Hu <matt.hu017@gmail.com>
2020-07-30 19:36:22 -0400

    Changed form.yml to include more values instead of hard-coding limits into form.js

--- form.js
+++ form.js
@@ -1,57 +1,49 @@
 'use strict'
 
 /**
+ * Clamp between two numbers
+ *
+ * @param      {number}  min     The minimum
+ * @param      {number}  max     The maximum
+ * @param      {number}  val     The value to clamp
+ */
+function clamp(min, max, val) {
+  let value = Math.min(max, Math.max(min, val));
+  return value;
+}
+
+/**
  * Fix num cores, allowing blanks to remain
  */
 function fix_num_cores() {
   let node_type_input = $('#batch_connect_session_context_node_type');
-  let node_type = node_type_input.val();
-  let num_cores_input = $('#num_cores');
+  let num_cores_input = $('#batch_connect_session_context_num_cores');
 
   if(num_cores_input.val() === '') {
     return;
   }
 
-  if(node_type === 'hugemem') {
-    set_ppn_owens_hugemem(num_cores_input);
-  } else {
-    set_ppn_owens_regular(num_cores_input);
-  }
+  set_ppn_by_node_type(node_type_input, num_cores_input);
 }
 
-/**
- * Sets the PPN limits available for Owens hugemem nodes.
- *
- * hugemem reservations are always assigned the full node
- *
- * @param      {element}  num_cores_input  The input for num_cores
- */
-function set_ppn_owens_hugemem(num_cores_input) {
-  const NUM_CORES = 48;
-  num_cores_input.attr('max', NUM_CORES);
-  num_cores_input.attr('min', NUM_CORES);
-  num_cores_input.val(NUM_CORES);
-}
+function set_ppn_by_node_type(node_type_input, num_cores_input){
+  let data = node_type_input.find(':selected').data();
 
-/**
- * Sets the PPN limits available for non hugemem Owens nodes.
- *
- * @param      {element}  num_cores_input  The input for num_cores
- */
-function set_ppn_owens_regular(num_cores_input) {
-  const NUM_CORES = 28;
-  num_cores_input.attr('max', NUM_CORES);
-  num_cores_input.attr('min', 0);
-  num_cores_input.val(Math.min(NUM_CORES, num_cores_input.val()));
-}
+  num_cores_input.attr('max', data.maxPpn);
+  num_cores_input.attr('min', data.minPpn);
 
+  // Clamp value between min and max
+  num_cores_input.val(
+    clamp(data.minPpn, data.maxPpn, num_cores_input.val())
+  );
+}
 
 /**
  * Change the maximum number of cores selected.
  */
 function set_node_type_change_handler() {
   let node_type_input = $('#batch_connect_session_context_node_type');
-  node_type_input.change(node_type_input, fix_num_cores);
+  node_type_input.change(fix_num_cores);
 }
 
 /**
@@ -106,25 +98,27 @@ function toggle_gpu_nodes(event){
  */
 function toggle_visibilty_of_form_group(form_id, show) {
   let form_element = $(form_id);
-  let parent = form_element;
-
-  while (
-    (! parent[0].classList.contains('form-group')) &&
-    (! parent.is('html')) // ensure that we don't loop infinitely
-  ) {
-    parent = parent.parent();
-  }
-
-  // If parent is HTML then something has gone wrong and visibility should not be changed
-  if ( parent.is('html') ) {
-    return;
-  }
+  let parent = form_element.parent();
 
   if(show) {
     parent.show();
   } else {
+    form_element.val('');
     parent.hide();
   }
+}
+
+function set_cluster_change_handler(){
+  let cluster_input = $('#batch_connect_session_context_cluster');
+  cluster_input.change(toggle_node_type);
+}
+
+function toggle_node_type(){
+  let all_options = $("#batch_connect_session_context_node_type option");
+  all_options.hide();
+  let cluster = $('#batch_connect_session_context_cluster').val();
+  let cluster_options = $("#batch_connect_session_context_node_type option[data-cluster=" + cluster + "]");
+  cluster_options.show();
 }
 
 // Main
@@ -135,5 +129,9 @@ toggle_tutorial_control_visibility(
   // Fake the event
   { target: document.querySelector('#batch_connect_session_context_version') }
 );
-set_node_type_change_handler();
+toggle_node_type();
+
+
+set_cluster_change_handler();
 set_version_change_hander();
+set_node_type_change_handler();
